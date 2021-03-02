42e3e8e2b6ff5fe04f915800508f23880ed182ff
Lewis Matos <lewiismb1@gmail.com>
2021-03-02 17:04:43 -0400

    alfa 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
+import { MusicCardComponent } from './components/music-card/music-card.component';
 
 const routes: Routes = [
 
   {path: 'home', component: HomeComponent},
   {path: 'search', component: SearchComponent},
   {path: 'artista', component: ArtistaComponent},
+  {path: 'prueba-estatica', component: MusicCardComponent},
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
 ];
